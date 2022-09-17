c0fc1f88dc15ffadcd48e917d61615f2d8b1f581
Sawalito <rojas.vazquez.saul.ubaldo@gmail.com>
2022-09-16 21:28:25 -0500

    Primera version

--- app/src/main/java/com/example/mysaul6/MainActivity.java
+++ app/src/main/java/com/example/mysaul6/MainActivity.java
@@ -1,57 +1,66 @@
 package com.example.mysaul6;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-    private static final String Tag = "Mi actividad";
+    private static final String Tag = "Activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(Tag,"OnCreate");
+        Log.i(Tag,"onCreate");
         TextView textView = findViewById(R.id.TextView1);
-        textView.setText("OnCreate");
+        textView.setText("onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(Tag,"OnStart");
+        Log.i(Tag,"onStart");
         TextView textView = findViewById(R.id.TextView1);
-        textView.setText("OnStart");
+        textView.setText("onStart");
+
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(Tag,"OnRestart");
+        Log.i(Tag,"onRestart");
+        TextView textView = findViewById(R.id.TextView1);
+        textView.setText("onRestart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(Tag,"OnPause");
+        Log.i(Tag,"onPause");
+        TextView textView = findViewById(R.id.TextView1);
+        textView.setText("onPause");
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(Tag,"OnDestroy");
+        Log.i(Tag,"onDestroy");
+        TextView textView = findViewById(R.id.TextView1);
+        textView.setText("onDestroy");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(Tag,"OnStop");
+        Log.i(Tag,"onStop");
+        TextView textView = findViewById(R.id.TextView1);
+        textView.setText("onStop");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(Tag,"onResume");
+        TextView textView = findViewById(R.id.TextView1);
+        textView.setText("onResume");
     }
 }
